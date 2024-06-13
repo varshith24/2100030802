cbb0634b0805c5d3d1e37e6ebc79e0752aa9a2fe
Chavalam <varshi1188@gmail.com>
2024-06-13 12:06:11 +0530

    Completed Task

--- AverageCalculator/server.js
+++ AverageCalculator/server.js
@@ -1,35 +1,54 @@
 const express = require('express');
 const axios = require('axios');
+const getToken = require('./BearerToken');
 const app = express();
-const getToken = require('./BearerToken'); // Ensure this file exports a function that returns a token
 const PORT = 9876;
 
+const type = {
+    "p": "primes",
+    "e": "even",
+    "f": "fibo",
+    "o": "odd",
+    "r": "rand"
+}
+
 let token;
+const previousRequests = [];
 
 const initializeToken = async () => {
     try {
         token = await getToken();
-        // console.log('Token fetched:', token);
     } catch (error) {
         console.error('Error fetching token:', error.message || error);
     }
 };
-initializeToken();
 
-app.get("/test/p", async (req, res) => {
+initializeToken()
+
+app.get("/test/:id", async (req, res) => {
+    const id = req.params.id;
+
     if (!token) {
         res.status(500).send("Token is not available");
         return;
     }
 
     try {
-        const response = await axios.get('http://20.244.56.144/test/primes', {
+        const response = await axios.get(`http://20.244.56.144/test/${type[id]}`, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
         });
-        // console.log(response.data);
-        res.status(200).json(response.data);
+
+        let s = 0;
+        for (let i = 0; i < response.data.numbers.length; i++) {
+            s += response.data.numbers[i];
+        }
+        const avg = s / response.data.numbers.length;
+
+        console.log(response.data);
+        res.status(200).json({ "windowPrevState": previousRequests.length == 0 ? [] : previousRequests[previousRequests.length - 1], "windowCurrState": response.data, "avg": avg });
+        previousRequests.push(response.data.numbers)
     } catch (e) {
         console.error('Error:', e.message || e);
         res.status(500).send("Error fetching data");
